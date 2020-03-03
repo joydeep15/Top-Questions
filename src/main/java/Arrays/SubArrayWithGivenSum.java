ff9bd38d3e62211007d79289a300162219d9be96
Joydeep <joydeep.yx@gmail.com>
2020-03-03 22:57:27 +0530

    Subarray With Given Sum

---
+++ src/main/java/Arrays/SubArrayWithGivenSum.java
@@ -0,0 +1,54 @@
+package Arrays;
+
+import java.util.*;
+
+public class SubArrayWithGivenSum {
+    static List<Integer> inputList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t!=0){
+             t--;
+            int len = in.nextInt();
+            int target = in.nextInt();
+            inputList.clear();
+            for(int i=0; i< len ;i++){
+                inputList.add(in.nextInt());
+            }
+            printSubArrayWithGivenSum(inputList, target);
+        }
+    }
+
+    private static void printSubArrayWithGivenSum(List<Integer> inputList, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int cumulative_sum = 0;
+        int start = 0;
+        int end = 0;
+
+        for(int i=0; i < inputList.size() ; i++){
+
+            cumulative_sum += inputList.get(i);
+            if(cumulative_sum == target){
+                //answer is 0,i
+                start = 1;
+                end = i + 1;
+                System.out.println( start+" "+end );
+                return;
+            }
+
+            if(map.containsKey(cumulative_sum - target)){
+
+                start = map.get(cumulative_sum-target) +1+1;
+                end = i+1;
+                System.out.println( start+" "+end );
+                return;
+            }
+
+            map.put(cumulative_sum, i);
+        }
+
+        System.out.println(-1);
+
+    }
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
