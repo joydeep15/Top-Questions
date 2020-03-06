d0fe3fd4d00bb358ad0995b0a1c09c87f9ae60c9
Joydeep <joydeep.yx@gmail.com>
2020-03-06 15:21:54 +0530

    Graph support

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,8 +1,18 @@
-import java.util.Scanner;
+import utilities.Graph;
 
 public class Runner {
 
     public static void main(String[] args) {
+
+        Graph<Integer> graph = new Graph<>();
+        graph.addEdge(0, 1, false);
+        graph.addEdge(0, 2, false);
+        graph.addEdge(0, 3, false);
+        graph.addEdge(3, 5, false);
+        graph.addEdge(2, 4, false);
+        graph.addEdge(2, 1, false);
+
+        System.out.println(graph.toString());
 
     }
 
---
+++ src/main/java/utilities/Graph.java
@@ -0,0 +1,66 @@
+package utilities;
+
+import java.util.*;
+
+public class Graph<T> {
+
+    private Map<T, Set<T>> graphStore = new HashMap<>();
+
+    public void addVertex(T vertex){
+
+        if( ! graphStore.containsKey(vertex) )
+            graphStore.put(vertex, new HashSet<T>());
+
+    }
+
+    public void addEdge(T src, T target, boolean bidirectional){
+
+        if(!graphStore.containsKey(src)) {
+            addVertex(src);
+        }
+
+        if(!graphStore.containsKey(target)) {
+            addVertex(target);
+        }
+
+        graphStore.get(src).add(target);
+        if(bidirectional){
+            graphStore.get(target).add(src);
+        }
+    }
+
+    public int getVertexCount(){
+        return graphStore.keySet().size();
+    }
+
+    public boolean hasVertex(T vertex){
+        return graphStore.containsKey(vertex);
+    }
+
+    public boolean hasEdge(T src, T target){
+        return graphStore.get(src).contains(target);
+    }
+
+    public Set<T> getVertices(){
+        return graphStore.keySet();
+    }
+
+    public Iterator<T> getEdges(T vertex){
+        return graphStore.get(vertex).iterator();
+    }
+
+    public String toString(){
+
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<T, Set<T>> entry : graphStore.entrySet()){
+            sb.append(entry.getKey()).append(": ");
+            for(T value : entry.getValue()){
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+
+    }
+
+}
