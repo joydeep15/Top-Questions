d2588eaa1ae3993554b283eecda79f75e38e127f
Joydeep <joydeep.yx@gmail.com>
2020-03-12 23:53:27 +0530

    Merge Sort Completed

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,4 @@
+import sorting.MergeSort;
 import sorting.QuickSort;
 import utilities.Graph;
 
@@ -6,20 +7,17 @@ import java.util.Arrays;
 public class Runner {
 
     public static void main(String[] args) {
-//
-//        Graph<Integer> graph = new Graph<>();
-//        graph.addEdge(0, 1, false);
-//        graph.addEdge(0, 2, false);
-//        graph.addEdge(0, 3, false);
-//        graph.addEdge(3, 5, false);
-//        graph.addEdge(2, 4, false);
-//        graph.addEdge(2, 1, false);
-//
-//        System.out.println(graph.toString());
 
-        int[] arr = {9,7,8,7,6,2,5,1};
-        QuickSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
+//        int[] arr = {9,7,8,7,6,2,5,1};
+//        QuickSort.sort(arr);
+//        System.out.println(Arrays.toString(arr));
+
+        int[] arr1 = {9,7,8,7,6,2,5,1};
+        MergeSort mrs = new MergeSort();
+        mrs.createTempArray(arr1.length);
+        mrs.sort(arr1);
+        //        mrs.merge(arr1,0,0,arr1.length-1);
+        System.out.println(Arrays.toString(arr1));
     }
 
 
--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -3,22 +3,32 @@ package sorting;
 public class MergeSort {
 
     int[] temp;
+    public void createTempArray(int length){
+        temp = new int[length];
+    }
     public  void sort(int[] array){
-        temp = new int[array.length];
         mergeSort(array, 0, array.length-1);
     }
 
     private  void mergeSort(int[] array, int low, int high) {
         if(low < high){
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
             mergeSort(array, low, mid);
             mergeSort(array, mid+1, high);
             merge(array, low, mid, high);
         }
     }
 
-    private void merge(int[] array, int low, int mid, int high) {
+    public void merge(int[] array, int low, int mid, int high) {
         // sorted from low->mid & mid+1 -> high
+
+        int leftListCount = mid - low + 1;
+        int rightListCount = high - mid - 1;
+
+        if(leftListCount < 1 || rightListCount < 0){
+            return;
+        }
+
         for(int i=low; i<=mid; i++){
             temp[i] = array[i];
         }
@@ -26,6 +36,39 @@ public class MergeSort {
         for(int i=mid+1; i<=high; i++){
             temp[i] = array[i];
         }
+
+        int leftIterator = low;
+        int rightIterator = mid + 1;
+        int k;
+        for(k=low; k<= high && leftIterator <= mid && rightIterator <=high; k++){
+
+            if( temp[leftIterator] < temp[rightIterator]){
+                array[k] = temp[leftIterator];
+                leftIterator++;
+            }else{
+                array[k] = temp[rightIterator];
+                rightIterator++;
+            }
+        }
+
+        if(leftIterator <= mid){
+            //populate remaining members of the left list
+            while (leftIterator<=mid){
+                array[k] = temp[leftIterator];
+                k++;
+                leftIterator++;
+            }
+        }
+
+        if(rightIterator <= high){
+            while (rightIterator <= high){
+                array[k] = temp[rightIterator];
+                k++;
+                rightIterator++;
+            }
+        }
+
+        return;
         
     }
 
