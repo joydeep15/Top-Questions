921d90821471bd4c7c5c2c0b6f94fb450a22b826
Joydeep <joydeep.yx@gmail.com>
2020-09-29 09:32:14 +0530

    Sudoku part-1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,17 +9,4 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class Runner {
 
-    public static void main(String[] args) throws Exception {
-
-        NQueens nq = new NQueens();
-        nq.isValidPlacement(new ArrayList<String>() {{
-            add("1");
-            add("3");
-            add("2");
-            add("0");
-        }},0);
-
-    }
-
-
 }
--- src/main/java/backtracking/SolveSudoku.java
+++ src/main/java/backtracking/SolveSudoku.java
@@ -8,7 +8,32 @@ import java.util.Set;
 public class SolveSudoku {
 //    https://leetcode.com/problems/sudoku-solver/
 
-    public void solveSudoku(char[][] board) {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        solveSudoku(board);
+        printBoard(board);
+    }
+
+    private static void printBoard(char[][] board) {
+        for (int row = 0; row <= board.length; row++) {
+            for (int col = 0; col <= board[row].length; col++) {
+                System.out.print(board[row][col]);
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public static void solveSudoku(char[][] board) {
 
         if (board.length != 9) {
             return;
@@ -22,8 +47,8 @@ public class SolveSudoku {
             return;
         }
 
-        for (int row = 0; row <= board.length; row++) {
-            for (int col = 0; col <= board[row].length; col++) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
 
                 if (board[row][col] != '.') {
                     continue;
@@ -33,15 +58,21 @@ public class SolveSudoku {
                     //make the choice
                     board[row][col] = option.toCharArray()[0];
 
-                    //validate the choice
+                    //solve further
+                    solveSudoku(board);
                 }
+
+                //undo the choice as we have exhausted all options
+                // maybe its time to look into a different route?
+                board[row][col] = '.';
 
 
             }
         }
     }
 
-    private List<String> getOptions(char[][] board, int row, int col) {
+
+    private static List<String> getOptions(char[][] board, int row, int col) {
 
         Set<String> domain = new HashSet<String>();
         for (int i = 1; i <= 9; i++) {
@@ -49,7 +80,7 @@ public class SolveSudoku {
         }
 
         //remove row entries
-        for (int colIter = 0; colIter <= board[row].length; colIter++) {
+        for (int colIter = 0; colIter < board[row].length; colIter++) {
             String element = "" + board[row][colIter];
             if (domain.contains(element)) {
                 domain.remove(element);
@@ -57,16 +88,31 @@ public class SolveSudoku {
         }
 
         //remove col entries
-        for (int rowIter = 0; rowIter <= board.length; rowIter++) {
+        for (int rowIter = 0; rowIter < board.length; rowIter++) {
             String element = "" + board[rowIter][col];
             if (domain.contains(element)) {
                 domain.remove(element);
             }
         }
+
+        //1 index instead of 0 index
+        row++;
+        col++;
+
+        for (int i = Math.floorDiv(row, 3)*3; i <= Math.ceil((double) row / 3)*3; i++) {
+            for (int j = Math.floorDiv(col, 3)*3; j <= Math.ceil((double) col / 3)*3; j++) {
+                String element = "" + board[i - 1][j - 1];
+
+                if (domain.contains(element)) {
+                    domain.remove(element);
+                }
+            }
+        }
+
         return new ArrayList<>(domain);
     }
 
-    private boolean boardComplete(char[][] board) {
+    private static boolean boardComplete(char[][] board) {
 
         for (int row = 0; row <= board.length; row++) {
             for (int col = 0; col <= board[row].length; col++) {
