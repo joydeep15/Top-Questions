501931bef4d30dd14a721345a3bfa46e6b70e0e9
Joydeep <joydeep.yx@gmail.com>
2020-09-30 21:16:53 +0530

    Sudoku continued

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,9 +4,28 @@ import utilities.ResourceReader;
 import utilities.URLDecode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Runner {
+    public static int compare(Integer o1, Integer o2) {
+        return o2-o1;
+    }
 
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> p = new PriorityQueue<Integer>((a,b) -> b-a){{
+            add(1);
+            add(257);
+            add(-12);
+        }};
+
+        while (!p.isEmpty()) {
+            System.out.println(p.poll());
+        }
+
+
+    }
 }
--- src/main/java/backtracking/SolveSudoku.java
+++ src/main/java/backtracking/SolveSudoku.java
@@ -20,17 +20,28 @@ public class SolveSudoku {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
+        char[][] solved = {
+                {'5', '3', '4', '6', '7', '8', '9', '.', '.'},
+                {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+                {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
+                {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+                {'4', '2', '6', '8', '5', '3', '7', '9', '.'},
+                {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+                {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+                {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+                {'3', '4', '5', '2', '8', '6', '1', '7', '9'}
+        };
         solveSudoku(board);
-        printBoard(board);
     }
 
     private static void printBoard(char[][] board) {
-        for (int row = 0; row <= board.length; row++) {
-            for (int col = 0; col <= board[row].length; col++) {
-                System.out.print(board[row][col]);
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print(board[row][col]+" ");
             }
-            System.out.println("\n");
+            System.out.println("");
         }
+        System.out.println("");
     }
 
     public static void solveSudoku(char[][] board) {
@@ -44,6 +55,7 @@ public class SolveSudoku {
         }
 
         if (boardComplete(board)) {
+            printBoard(board);
             return;
         }
 
@@ -95,12 +107,12 @@ public class SolveSudoku {
             }
         }
 
-        //1 index instead of 0 index
+        //1 indexing
         row++;
         col++;
 
-        for (int i = Math.floorDiv(row, 3)*3; i <= Math.ceil((double) row / 3)*3; i++) {
-            for (int j = Math.floorDiv(col, 3)*3; j <= Math.ceil((double) col / 3)*3; j++) {
+        for (int i = Math.floorDiv(row, 3)*3 + 1; i <= Math.ceil((double) row / 3)*3; i++) {
+            for (int j = Math.floorDiv(col, 3)*3 + 1; j <= Math.ceil((double) col / 3)*3; j++) {
                 String element = "" + board[i - 1][j - 1];
 
                 if (domain.contains(element)) {
@@ -114,8 +126,8 @@ public class SolveSudoku {
 
     private static boolean boardComplete(char[][] board) {
 
-        for (int row = 0; row <= board.length; row++) {
-            for (int col = 0; col <= board[row].length; col++) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
                 if (board[row][col] == '.') {
                     return false;
                 }
